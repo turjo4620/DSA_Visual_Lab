fd51d95e4fcff8f52e43015e2ba2fdece91d95c0
turjo4620 <sarkerturjo2022@gmail.com>
2026-03-19 03:40:11 +0600

    added

--- src/main/java/com/example/dsa_visual_lab/controller/home/HomeController.java
+++ src/main/java/com/example/dsa_visual_lab/controller/home/HomeController.java
@@ -8,15 +8,12 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-//import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 
 public class HomeController {
 
     private final String CSS_PATH = "/com/example/dsa_visual_lab/view/styles/home.css";
-
-
 
     @FXML
     public void initialize() {
@@ -27,80 +24,62 @@ public class HomeController {
     private void openSorting(ActionEvent event) {
         try {
             Parent sortingRoot = FXMLLoader.load(getClass().getResource("/com/example/dsa_visual_lab/view/Sorting/sorting-view.fxml"));
-
-            Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-            Scene scene = stage.getScene();
-
-
+            Scene scene = ((Node) event.getSource()).getScene();
             scene.setRoot(sortingRoot);
-
 
             scene.getStylesheets().clear();
             scene.getStylesheets().add(getClass().getResource(CSS_PATH).toExternalForm());
-
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
 
     @FXML
     private void handleBack(ActionEvent event) {
         try {
             Parent homeRoot = FXMLLoader.load(getClass().getResource("/com/example/dsa_visual_lab/view/home/home-view.fxml"));
-
-            Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-            Scene scene = stage.getScene();
-
-
+            Scene scene = ((Node) event.getSource()).getScene();
             scene.setRoot(homeRoot);
-
 
             scene.getStylesheets().clear();
             scene.getStylesheets().add(getClass().getResource(CSS_PATH).toExternalForm());
-
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-
     @FXML
-    protected void openDataStructures(ActionEvent event) throws IOException {
-
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-
-
-        double width = stage.getWidth();
-        double height = stage.getHeight();
-        boolean isMaximized = stage.isMaximized();
-
-
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/dsa_visual_lab/view/Linear-DataStructure/linear-dataStructures.fxml"));
-
-        Scene scene = new Scene(fxmlLoader.load());
-
-
-        stage.setScene(scene);
-
-
-        stage.setWidth(width);
-        stage.setHeight(height);
-        stage.setMaximized(isMaximized);
-
-        stage.show();
+    protected void openDataStructures(ActionEvent event) {
+        try {
+            Parent linearRoot = FXMLLoader.load(getClass().getResource("/com/example/dsa_visual_lab/view/Linear-DataStructure/linear-dataStructures.fxml"));
+            Scene scene = ((Node) event.getSource()).getScene();
+            scene.setRoot(linearRoot);
+        } catch (IOException e) {
+            System.out.println("Error loading Linear Data Structures View:");
+            e.printStackTrace();
+        }
     }
 
     @FXML
     public void openTrees(ActionEvent event) {
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/dsa_visual_lab/view/BST/bst-view.fxml"));
-            Scene scene = new Scene(fxmlLoader.load());
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            stage.setScene(scene);
-            stage.show();
+            Parent bstRoot = FXMLLoader.load(getClass().getResource("/com/example/dsa_visual_lab/view/BST/bst-view.fxml"));
+            Scene scene = ((Node) event.getSource()).getScene();
+            scene.setRoot(bstRoot);
         } catch (IOException e) {
             System.out.println("Error loading BST View:");
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void openDP(ActionEvent event) {
+        try {
+            Parent dpRoot = FXMLLoader.load(getClass().getResource("/com/example/dsa_visual_lab/view/dp/knapsack-view.fxml"));
+            Scene scene = ((Node) event.getSource()).getScene();
+            scene.setRoot(dpRoot);
+        } catch (IOException e) {
+            System.out.println("Error loading Knapsack View:");
             e.printStackTrace();
         }
     }
@@ -113,11 +92,6 @@ public class HomeController {
     @FXML
     public void openMST() {
         showFeatureAlert("Minimum Spanning Trees");
-    }
-
-    @FXML
-    public void openDP() {
-        showFeatureAlert("Dynamic Programming");
     }
 
     private void showFeatureAlert(String featureName) {
